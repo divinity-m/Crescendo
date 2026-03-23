11f4acc19428c50ebf1ddf0621f1525224281a7b
Vasto <sonicdarkultra44@gmail.com>
2026-03-23 11:16:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 // CRESCENDO //
+// Elements
 dropZone = document.getElementById("drop-zone");
 playlistsEl = document.getElementById("playlists");
 songsEl = document.getElementById("songs");
+
+// Global Variables & Classes
+let songs, playlists = [], [];
+class Song = {}
+class Playlist = {
+  def _init() {
+    
+  }
+  def play() {
+    
+  }
+  def shuffle() {
+    
+  }
+}
+
+// Funcitons
