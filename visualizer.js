d8f5f7a4f3c86532ab5837f7bf2e203fa1aed693
Vasto <sonicdarkultra44@gmail.com>
2026-04-04 20:11:56 -0600

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -1,1 +1,31 @@
+/// CRESCENDO VISUALIZER.JS ///
+/* Canvas Setup */
+const visualizer = document.getElementById("music-visualizer");
+const ctx = visualizer.getContext("2d");
+const cnvWidth = visualizer.width;
+const cnvHeight = visualizer.height;
 
+let x = 10;
+let dx = 1;
+
+
+requestAnimationFrame(draw);
+function draw() {
+    ctx.clearRect(0, 0, cnvWidth, cnvHeight);
+    
+    ctx.fillStyle = "oklch(48.8% 0.243 264.376)";
+    ctx.lineWidth = 1.5;
+    ctx.font = "2rem outfit";
+    ctx.textAlign = "center";
+    ctx.fillText("Music Visualizer", cnvWidth/2, cnvHeight/2);
+    
+    ctx.fillRect(x, cnvHeight/2, 10, 10);
+    if (x >= cnvWidth - 10) dx = -1;
+    if (x <= 0) dx = 1;
+
+    
+    x += dx;
+    
+    requestAnimationFrame(draw);
+}
+draw();
